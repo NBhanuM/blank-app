a80064d09c857c8759700ca742abafa5018c2c98
NBhanuM <32984000+NBhanuM@users.noreply.github.com>
2024-08-17 23:42:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
 import streamlit as st
 
 st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+def main():
+    uploaded_file = st.file_uploader("Choose an excel file")
+    if uploaded_file is not None:
+        try:
+            st.write(f"Uploaded file: {uploaded_file.name}, Type: {uploaded_file.type}")
+            file_contents = uploaded_file.read()
+            df = pd.read_excel(uploaded_file)
+            
+        except Exception as e:
+            st.error(f"Error processing file: {e}")
+    st.write('File not uploaded')
+
+if __name__ == "__main__":
+    main()
